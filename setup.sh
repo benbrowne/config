3136f70437a76af6ba9ee53b11b0b1b5dc5049d4
Ben Browne <ben.browne@kwhanalytics.com>
2021-06-18 15:54:09 +0000

    do not send a \n to coc-pyright install

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ curl -sL install-node.now.sh/lts | bash -s -- -f
 echo 'installing vim plugins'
 vi +'PlugInstall' +'qa'
 echo 'installing COC-pyright'
-printf '\n' | vi +'CocInstall coc-pyright' +'qa'
+vi +'CocInstall coc-pyright' +'qa'
 
 # python module doq is a dependency for vim-pydocstring
 pip install doq 
