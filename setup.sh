41f5e0a0506e8d4b14447cca3a48a85fcba3f0f2
Ben Browne <ben.browne@kwhanalytics.com>
2021-08-09 12:17:38 -0700

    remove coc-pyright: not working

--- setup.sh
+++ setup.sh
@@ -1,20 +1,11 @@
-# Install vim 8.2 for COC compatibility
-add-apt-repository ppa:jonathonf/vim -y
-apt update
-apt install vim -y
-
 # Fuzzy finder
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --completion --key-bindings --update-rc
 
 echo 'installing vimplug'
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo 'installing Nodejs for COC-pyright'
-curl -sL install-node.now.sh/lts | bash -s -- -f
 echo 'installing vim plugins'
 vi +'PlugInstall' +'qa'
-echo 'installing COC-pyright'
-vi +'CocInstall coc-pyright'
 
 # python module doq is a dependency for vim-pydocstring
 pip install doq 
