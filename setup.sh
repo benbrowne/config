b41cae3ef00989fca1ce0513d739e6c970039bfd
Ben Browne <ben.browne@kwhanalytics.com>
2024-09-25 12:04:24 +0100

    get setup.sh script working seamlessly on linux

--- setup.sh
+++ setup.sh
@@ -1,37 +1,40 @@
 # Fuzzy finder
+echo 'installing fzf (fuzzy finder)'
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --completion --key-bindings --update-rc
 
 # Neovim
-apt-get update
+echo 'installing Neovim'
+apt update
 add-apt-repository -y ppa:neovim-ppa/stable
-apt-get update
+apt update
 apt install -y neovim
 
-# aliases maps vi to nvim so must come after neovim install.
-source .bash_aliases
+# aliases maps vi to nvim so must come after neovim install
+sudo -u ben source ~/.bash_aliases
 
-echo 'installing vimplug'
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+# Vim Plug installation
+echo 'installing vim-plug'
+sudo -u ben sh -c 'curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-echo 'installing Nodejs for COC-pyright'
+
+# Install Node.js
+echo 'installing Node.js for COC-pyright'
 curl -sL install-node.now.sh/lts | bash -s -- -f
+
+# Installing vim plugins as user 'ben'
 echo 'installing vim plugins'
-nvim +'PlugInstall' +'CocInstall -sync coc-pyright coc-sql coc-yaml' # CocInstalls don't work with +'qall'
+sudo -u ben nvim +'PlugInstall' +'CocInstall -sync coc-pyright coc-sql coc-yaml' +qall
+
+# System packages (rename and ag)
 echo 'installing rename'
-apt install rename
+apt install -y rename
+
 echo 'installing ag'
-apt install silversearcher-ag
+apt install -y silversearcher-ag
 
-# autoformatting
-pip install autopep8
-
-# Linting
-pip install flake8
-
+# Python tools
+echo 'installing Python packages'
 # python module doq is a dependency for vim-pydocstring
-pip install doq 
-
-# Prefer to pdb for debugging.
-pip install ipdb
+pip install autopep8 flake8 doq ipdb
 
